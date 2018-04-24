6fee4135270a700e10fddfb80aa13ab05b8bd1f9
samku <sampath.kumar.mulagaleti@gmail.com>
2018-04-24 14:07:18 +0200

    Working Q learning with value gradients

--- exercises_functions.m
+++ exercises_functions.m
@@ -2,24 +2,24 @@ clc
 clear all
 
 %System dynamics
-a1 = 0.5;
+a1 = 0.4;
 b1 = 1.0;
-a2 = 0.5;
+a2 = 0.4;
 b2 = b1;
 p = 1.0;
 
 %Parameters
 init_state = 10;
 final_state = 0;
-N_steps = 500;
-N_episodes = 1000;
+N_steps = 50;
+N_episodes = 100;
 gamma = 1.0;
 epsilon = 0.001;
 factor = 1.0;
 
 %Cost weights
 Q = 1;
-R = 2;
+R = 0.001;
 [K,~,~] = dlqr(a1,b1,Q,R,0);
 x_sim = init_state;
 for i=2:N_steps
@@ -27,10 +27,10 @@ for i=2:N_steps
 end
 
 %Initializtion
-H = rand(2,2);
+H = ones(2,2);
 
 %Type
-type = 3;
+type = 4;
 %1 - MC, 2- MC, RLS
 
 if type == 1
@@ -77,7 +77,6 @@ if type == 1
                 H_elements = H_elements + S\(X_N'*(Y_N - X_N*H_elements));
             end
         end
-        %         H_elements = matrix\vector;
         H = [H_elements(1) H_elements(2); H_elements(2) H_elements(3)];
         K_vec = [ K_vec;(H(2,1)/H(2,2))];
         K_vec(end);
@@ -170,7 +169,7 @@ if type == 3
         x(i,1) = init_state;
         for j=2:N_steps
             %Apply to dynamics - Observe next state
-            u(i,j-1) = (H(2,1)/H(2,2))*(final_state-x(i,j-1))+0.02*randn(1,1);
+            u(i,j-1) = (H(2,1)/H(2,2))*(final_state-x(i,j-1))+0.001*randn(1,1);
             K_vec = [ K_vec;(H(2,1)/H(2,2))];
             if x(i,j-1)<=2.5
                 x(i,j) = a1*x(i,j-1)^p+b1*u(i,j-1);
@@ -179,26 +178,32 @@ if type == 3
             end
         end
         K_vec(end)
+        subplot(2,3,3)
         scatter(i,K_vec(end))
         hold on
         scatter(i,K,'.')
         hold on
         pause(0.001)
+% --------------------------------------------------------------------
+%Learning phase
         %Use every visit MC
-        %Use complete gradient descent
-        H_vector = [H(1,1);H(1,2);H(2,1);H(2,2)];
-        for gd_iter = 1:100
-            %Calculate gradient at evaluated point
-            dH_sum = zeros(4,1);
-            for j=1:N_steps-1
-                %Calculate total cost till end
+        %Calculate cost targets first
+        for j=1:N_steps-1
                 net_cost(j) = 0;
                 for k=j:N_steps
                     error_k = final_state - x(i,k);
                     net_cost(j) = net_cost(j) + gamma^(k-j)*(Q*error_k^2+R*u(i,k)^2);
                 end
+        end
+        %Use complete gradient descent
+        H_vector = [H(1,1);H(1,2);H(2,1);H(2,2)];
+        for gd_iter = 1:10
+            %Calculate gradient at evaluated point
+            dH_sum = zeros(4,1);
+            for j=1:N_steps-1
+                %For ease of notation
                 matrix = [x(i,j)*x(i,j)' x(i,j)*u(i,j)' u(i,j)*x(i,j)' u(i,j)*u(i,j)'];
-                %dH
+                %Enumerate gradient
                 dH_sum = dH_sum+(matrix*H_vector-net_cost(j))*matrix';
             end
             %Calculate optimal step size at the current point
@@ -214,6 +219,19 @@ if type == 3
             alpha = [alpha num/den];
             %Take a gradient step
             H_vector = H_vector+alpha(end)*dH_sum;
+            subplot(2,3,1)
+            scatter(gd_iter,H_vector(1),'.')
+            hold on
+            subplot(2,3,2)
+            scatter(gd_iter,H_vector(2),'.')
+            hold on
+            subplot(2,3,4)
+            scatter(gd_iter,H_vector(3),'.')
+            hold on
+            subplot(2,3,5)
+            scatter(gd_iter,H_vector(4),'.')
+            hold on
+            pause(0.001)
         end
         %Put back in matrix
         H(1,1) = H_vector(1);
@@ -227,9 +245,10 @@ if type == 3
         end
         cost_MC = [cost_MC cost];
         %Check cost
-%         scatter(i,cost)
-%         hold on
-%         pause(0.0001)
+        subplot(2,3,6)
+        scatter(i,cost)
+        hold on
+        pause(0.0001)
     end
 end
 
@@ -246,9 +265,10 @@ if type == 4
         x(i,1) = init_state;
         for j=2:N_steps
             %Apply to dynamics - Observe next state
-            u(i,j-1) = (H(2,1)/H(2,2))*(final_state-x(i,j-1))+0.5*randn(1,1);
+            u(i,j-1) = (H(2,1)/H(2,2))*(final_state-x(i,j-1))+0.001*randn(1,1);
             K_vec = [ K_vec;(H(2,1)/H(2,2))];
 %             %-----
+            subplot(2,3,3)
             scatter(j,K_vec(end))
             hold on
             pause(0.001)
@@ -262,7 +282,7 @@ if type == 4
             H_vector = [H(1,1);H(1,2);H(2,1);H(2,2)];
             error = final_state - x(i,j-1);
             error_next = final_state - x(i,j);
-            matrix = [error^2 u(i,j-1)*error u(i,j-1)*error u(i,j-1)^2];
+            matrix = [x(i,j-1)^2 u(i,j-1)*x(i,j-1) u(i,j-1)*x(i,j-1) u(i,j-1)^2];
             vector = (Q*error^2+R*u(i,j-1)^2) + gamma*[error_next u(i,j)]*H*[error_next;u(i,j)]; %TD(0) update
             %Sample gradient of the fitting problem
             gradient = (matrix*H_vector - vector)*matrix';
@@ -277,6 +297,20 @@ if type == 4
             H(1,2) = H_vector(2);
             H(2,1) = H_vector(3);
             H(2,2) = H_vector(4);
+            %Plot
+            subplot(2,3,1)
+            scatter(j,H_vector(1),'.')
+            hold on
+            subplot(2,3,2)
+            scatter(j,H_vector(2),'.')
+            hold on
+            subplot(2,3,4)
+            scatter(j,H_vector(3),'.')
+            hold on
+            subplot(2,3,5)
+            scatter(j,H_vector(4),'.')
+            hold on
+            pause(0.001)
         end
         %Compute LQ cost
         cost  =0;
@@ -285,9 +319,10 @@ if type == 4
         end
         cost_MC = [cost_MC cost];
         %Check cost
-%                 scatter(i,cost)
-%                 hold on
-%                 pause(0.0001)
+        subplot(2,3,6)
+        scatter(i,cost)
+        hold on
+        pause(0.0001)
     end
 end
 
